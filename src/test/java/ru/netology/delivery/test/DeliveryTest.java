f12a0f518888b4d2adaa092ba61908699e7e3dc7
Sergey <mcmserg@mail.ru>
2026-09-05 12:57:13 +0300

    Added Allure Report

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -2,9 +2,10 @@ package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -14,6 +15,16 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
+
+    @BeforeAll
+    static void setupAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
@@ -59,6 +70,54 @@ class DeliveryTest {
         $("[data-test-id='success-notification'] .notification__content")
                 .should(visible, Duration.ofSeconds(15))
                 .should(Condition.text("Встреча успешно запланирована на " + secondMeetingDate));
+
+    }
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldNegativeTesting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.exactText("Запрограммировать")).click();
+        $("[data-test-id='success-notification'] .notification__title")
+                .should(visible)
+                .should(Condition.text("Успешно!"));
+        $("[data-test-id='success-notification'] .notification__content")
+                .should(visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
+
+    }
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldNegativeTestingV2() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
+        $("[data-test-id=name input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.exactText("Запрограммировать")).click();
+        $("[data-test-id='success-notification'] .notification__title")
+                .should(visible)
+                .should(Condition.text("Успешно!"));
+        $("[data-test-id='success-notification'] .notification__content")
+                .should(visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
 
     }
 }
